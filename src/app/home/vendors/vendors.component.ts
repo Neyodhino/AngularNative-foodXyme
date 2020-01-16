791afcae5edcd3a92af3199ff035a62e05534387
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-01-16 12:10:19 +0100

    Feat: Vendors component created and added to the appropriate module and routing

---
+++ src/app/home/vendors/vendors.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: "Vendors",
+    templateUrl: "./vendors.component.html"
+})
+export class VendorsComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+}
+
