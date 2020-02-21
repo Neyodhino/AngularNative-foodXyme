984d242ab3fb88d8e8f994aab245e142e219b558
Olaniyi Awofisayo <olaniyiawofisayo@gmail.com>
2020-02-21 19:26:20 +0100

    Fixed: Add to cart functionality has been implemented

--- src/app/shared/Modal/cartDialog.component.ts
+++ src/app/shared/Modal/cartDialog.component.ts
@@ -1,49 +1,61 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ModalDialogParams } from "nativescript-angular/modal-dialog";
+import * as appSettings from 'tns-core-modules/application-settings';
 
 @Component({
     selector: "cart-dialog",
     templateUrl: "./cartDialog.component.html",
     styleUrls: ["./cartDialog.component.scss"]
 })
-export class CartDialogComponent implements OnInit {
+export class CartDialogComponent {
 
     counter: number = 1;
     setFlag: boolean = true;
-    stockItemId: number;
-    stockItemPrice: number;
-    stockItemName: string;
     stockTotalPrice: number;
     stockItemDetails;
+    database: any;
     constructor(
         private params: ModalDialogParams
     ) {
-        // this.stockItemId = params.context.stockId;
-        // this.stockItemPrice = params.context.stockPrice;
-        // this.stockItemName = params.context.stockName;
-        // this.stockTotalPrice = this.stockItemPrice;
         this.stockItemDetails = params.context.stockItem;
-        console.log(this.stockItemDetails);
+        this.stockTotalPrice = this.stockItemDetails.stockprice;
     }
 
     increaseCounter() {
         this.counter = this.counter + 1;
-        this.stockTotalPrice = this.stockTotalPrice + this.stockItemPrice;
+        this.stockTotalPrice = this.stockTotalPrice + this.stockItemDetails.stockprice;
     }
     decreaseCounter() {
         if (this.counter > 1) {
             this.setFlag = false;
             this.counter = this.counter - 1;
-            this.stockTotalPrice = this.stockTotalPrice - this.stockItemPrice;
+            this.stockTotalPrice = this.stockTotalPrice - this.stockItemDetails.stockprice;
         } else {
             this.setFlag = true;
         }
     }
 
-    addToCart() {
-        console.log(this.stockTotalPrice);
+    query() {
+        console.log(appSettings.getString("cart"));
     }
 
-    ngOnInit(): void {}
+    addToCart(itemName, itemQuantity, itemPrice) {
+        let itemStored = appSettings.getString("cart");
+        let itemSelected = {
+            "itemName": itemName,
+            "itemQuantity": itemQuantity,
+            "itemPrice": itemPrice
+        };
+        if (itemStored == undefined) {
+            let dataStored = [];
+            dataStored.push(itemSelected);
+            let stringifyData = JSON.stringify(dataStored);
+            appSettings.setString("cart", stringifyData)
+        }else {
+            let serialisedData = JSON.parse(itemStored);
+            serialisedData.push(itemSelected);
+            let sd = JSON.stringify(serialisedData);
+            appSettings.setString("cart", sd);
+        }
+    }
 }
-
